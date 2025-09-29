a852a6c42b61fe8e62280e6648292e154528711f
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-29 17:48:10 +0800

    Add ProfileFooter component with social links and dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import Dashboard from "@/components/layout/Dashboard";
 
 export default function Home() {
   return (
     <div className="font-geneva flex flex-col items-center justify-items-center min-h-screen w-full gap-16">
-      <section id="dashboard" className="h-screen bg-blue-100 flex items-center justify-center w-full border" data-section="dashboard">
-        <h1 className="text-4xl">Dashboard Section</h1>
-      </section>
+      <Dashboard />
 
       <section id="projects" className="h-screen bg-green-100 flex items-center justify-center w-full" data-section="projects">
         <h1 className="text-4xl">Projects Section</h1>
