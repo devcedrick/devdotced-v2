61b4cc3c13b41ec66d4545fb3fe8c71301f1a4c7
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-30 21:58:32 +0800

    Adds contact section with "coming soon" placeholder
    Implements a new contact section with a placeholder indicating that it is currently under development.
    
    The contact section will eventually include a contact form, social links, and other interactive elements to facilitate communication.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Dashboard from "@/components/dashboard/Dashboard";
 import Project from "@/components/projects/Project";
 import Certificates from "@/components/certificates/Certificates";
+import Contact from "@/components/contact/Contact";
 
 export default function Home() {
   return (
@@ -11,9 +12,7 @@ export default function Home() {
 
       <Certificates />
 
-      <section id="contact" className="h-screen bg-red-100 flex items-center justify-center w-full" data-section="contact">
-        <h1 className="text-4xl">Contact Section</h1>
-      </section>
+      <Contact />
     </div>
   );
 }
